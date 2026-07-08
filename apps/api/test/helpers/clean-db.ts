0d082f3de8013aef258f842c6d592e464dea5a58
KendrickXu <a1131951753@gmail.com>
2026-07-08 21:22:37 +0800

    feat(api): add content publishing data model

--- apps/api/test/helpers/clean-db.ts
+++ apps/api/test/helpers/clean-db.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
 export async function cleanDb(prisma: PrismaClient) {
+  await prisma.adminAuditLog.deleteMany();
   await prisma.seriesUnlock.deleteMany();
   await prisma.membership.deleteMany();
   await prisma.episode.deleteMany();
